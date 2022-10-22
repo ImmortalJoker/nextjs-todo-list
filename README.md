6b66bde753d68bad4f289b7882eb5973dbe0ee4d
ImmortalJoker <krasnovegorinc@gmail.com>
2022-10-22 13:55:01 +0200

    add deps and add link to vercel

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+Link to the [production](https://nextjs-todo-list-sooty.vercel.app/)
 ## Getting Started
 
 First, run the development server:
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withTM = require('next-transpile-modules')([
+  '@ionic/react',
+  '@ionic/core',
+  '@stencil/core',
+  'ionicons',
+]);
 
-module.exports = nextConfig
+module.exports = withTM({
+  basePath: '',
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  swcMinify: true,
+});
